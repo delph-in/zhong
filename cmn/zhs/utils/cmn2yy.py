5b70703866b3e28198748f05eb4f56b1df3ee672
zhenzhen <zhenzhen@nus.edu.sg>
2018-03-30 12:41:43 +0800

    cleaning up numeral lex, coordination, and yy-mode postagging commands

--- cmn/zhs/utils/cmn2yy.py
+++ cmn/zhs/utils/cmn2yy.py
@@ -9,24 +9,24 @@
 ###     (ubuntu) sudo apt-get install default-jre
 ### [1] install NLTK
 ### [2] download Stanford POS Tagger
-###	http://nlp.stanford.edu/software/stanford-postagger-full-2014-08-27.zip
+###        http://nlp.stanford.edu/software/stanford-postagger-full-2014-08-27.zip
 ###     If you use java1.8, you have to download http://nlp.stanford.edu/software/stanford-postagger-full-2014-10-26.zip
 ### [3] extract the zip file somewhere you want. For me, its path is /home/sanghoun/tools/stanford-postagger/.
 ### [4] register the path in your bashrc or whatever. The name must be STANFORD_TAGGER_PATH.
-###	For example,
-###		export STANFORD_TAGGER_PATH=/home/sanghoun/tools/stanford-postagger
+###        For example,
+###                export STANFORD_TAGGER_PATH=/home/sanghoun/tools/stanford-postagger
 ### [5] either do [ $ source ~/.bashrc ] or
-###	logout and login again.
+###        logout and login again.
 
 import sys, os
 import nltk
-from nltk.tag.stanford import POSTagger
+from nltk.tag.stanford import StanfordPOSTagger
 
-if not os.environ.has_key("STANFORD_TAGGER_PATH"):
-	print("STANFORD_TAGGER_PATH is not registered.")
-	sys.exit()
+if "STANFORD_TAGGER_PATH" not in os.environ :
+        print("STANFORD_TAGGER_PATH is not registered.")
+        sys.exit()
 
-tagger = POSTagger(os.environ['STANFORD_TAGGER_PATH']+"/models/chinese-distsim.tagger", os.environ['STANFORD_TAGGER_PATH']+"/stanford-postagger.jar", encoding='utf-8') 
+tagger = StanfordPOSTagger(os.environ['STANFORD_TAGGER_PATH']+"/models/chinese-distsim.tagger", os.environ['STANFORD_TAGGER_PATH']+"/stanford-postagger.jar", encoding='utf-8') 
 
 while True:
         try:
@@ -35,47 +35,47 @@ while True:
             break
         if not line or len(line.strip()) < 1: break
 
-        sentence = line.decode('utf-8')
+        sentence = line
 
-	output = ""
-	#print words.strip().split()
-	words = sentence.split()
+        output = ""
+        #print words.strip().split()
+        words = sentence.split()
 
-	#This is a tentative solution for handling too long sentences.
-	#FIX ME!
-	if len(words) > 200: 
-		print('(0, 0, 1, <0:18>, 1, "TOO-LONG-SENTENCE", 0, "null", "X" 1.0000)')
-		sys.stdout.flush()
-		continue
+        #This is a tentative solution for handling too long sentences.
+        #FIX ME!
+        if len(words) > 200: 
+                print('(0, 0, 1, <0:18>, 1, "TOO-LONG-SENTENCE", 0, "null", "X" 1.0000)')
+                sys.stdout.flush()
+                continue
 
-	result = tagger.tag(words)
-	#print result
-	_num = _from = _from_c = 0
-	for w in result:
-		[orth, tag] = w[1].split('#')
+        result = tagger.tag(words)
+        #print result
+        _num = _from = _from_c = 0
+        for w in result:
+                [orth, tag] = w[1].split('#')
 
-		#This is also a tentative solution. FIX ME!
-		if orth == '"': orth = '”'.decode('utf-8')
+                #This is also a tentative solution. FIX ME!
+                if orth == '"': orth = '”'
 
-		_num += 1
-		output += '('
-		output += str(_num)
-		output += ', '
-		output += str(_from)
-		output += ', '
-		output += str(_from+1)
-		output += ', <'
-		output += str(_from_c)
-		output += ':'
-		output += str(len(orth))
-		output += '>, 1, "'
-		output += orth
-		output += '", 0, "null", "'
-		output += tag
-		output += '" 1.0000) '
-		_from += 1
-		_from_c += len(orth)
+                _num += 1
+                output += '('
+                output += str(_num)
+                output += ', '
+                output += str(_from)
+                output += ', '
+                output += str(_from+1)
+                output += ', <'
+                output += str(_from_c)
+                output += ':'
+                output += str(len(orth))
+                output += '>, 1, "'
+                output += orth
+                output += '", 0, "null", "'
+                output += tag
+                output += '" 1.0000) '
+                _from += 1
+                _from_c += len(orth)
 
-	print(''.join(output.strip()).encode('utf-8'))
-	sys.stdout.flush()
+        print(''.join(output.strip()))
+        sys.stdout.flush()
 
--- cmn/zhs/utils/yy.sh
+++ cmn/zhs/utils/yy.sh
@@ -1,5 +1,7 @@
-MYPATH=$(pwd)/utils
-[[ $(pwd) =~ ^.*utils$ ]] && MYPATH=$(pwd)
+MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $STANFORD_SEGMENTER_PATH/segment.sh ctb $1 UTF-8 0 2>/dev/null | java -mx2g -cp $STANFORD_TAGGER_PATH/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger -model $STANFORD_TAGGER_PATH/models/chinese-distsim.tagger 2>/dev/null | python $MYPATH/tag2yy.py
+
+
+#$ java -classpath "stanford-postagger.jar:lib/*" edu.stanford.nlp.tagger.maxent.MaxentTagger -prop /home/damiano/modelli/italian.tagger.props -model italian.tagger -trainFile italian.tagger.train
 
