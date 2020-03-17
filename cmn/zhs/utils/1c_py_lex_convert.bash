50b763136f794cb701bcb5feb46877f55563bb38
Luis Morgado da Costa <lmorgado.dacosta@gmail.com>
2020-03-17 15:38:13 +0800

    reorganized the lexicon a bit: removed lexicon-bound, which was not being used anymore; added lexicon-core which to help with ambiguity in educational settings; updated all the grammar files to reflect this change

--- cmn/zhs/utils/1c_py_lex_convert.bash
+++ cmn/zhs/utils/1c_py_lex_convert.bash
@@ -1,6 +1,6 @@
+python lexicon_split.py -f ../lexicon-core.tdl -o ../1c/lexicon-core.tdl
 python lexicon_split.py -f ../lexicon.tdl -o ../1c/lexicon.tdl
 python lexicon_split.py -f ../lex-base.tdl -o ../1c/lex-base.tdl
-python lexicon_split.py -f ../lex-bound.tdl -o ../1c/lex-bound.tdl
 python lexicon_split.py -f ../lex-classifiers.tdl -o ../1c/lex-classifiers.tdl
 python lexicon_split.py -f ../lex-numbers.tdl -o ../1c/lex-numbers.tdl
 python lexicon_split.py -f ../mal-lex.tdl -o ../1c/mal-lex.tdl
